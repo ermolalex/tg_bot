da980d4857905bbb4389b96165c475d210e1c8b1
ermolalex <alex@kik-soft.ru>
2025-07-22 22:59:10 +0300

    add view

--- app/config/views.py
+++ app/config/views.py
@@ -4,16 +4,19 @@ from django.http import JsonResponse
 from telegram import Update
 from telegram.ext import Application # Or your custom bot handler
 
+from utils.logger import create_logger
+
+logger = create_logger(logger_name=__name__)
+
+
 # Assuming 'application' is your python-telegram-bot Application instance
 # initialized elsewhere (e.g., in your bot's __init__.py or a separate module)
-
 @csrf_exempt
 async def telegram_webhook_view(request):
     if request.method == 'POST':
         try:
             update_data = json.loads(request.body)
-            update = Update.de_json(data=update_data, bot=application.bot)
-            await application.update_queue.put(update)
+            logger.info(update_data)
             return JsonResponse({"status": "ok"})
         except Exception as e:
             return JsonResponse({"status": "error", "message": str(e)}, status=400)
